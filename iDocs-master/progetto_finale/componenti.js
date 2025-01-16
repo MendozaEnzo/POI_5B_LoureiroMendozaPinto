b233f2db096eda9d6f94434f84ce0d397320da32
Mendoza Enzo <mendozawayne@itis-molinari.eu>
2025-01-16 12:17:27 +0100

    aggiunta navigator.js

--- iDocs-master/progetto_finale/componenti.js
+++ iDocs-master/progetto_finale/componenti.js
@@ -1,4 +1,4 @@
-//componente navigator
+// Funzioni per la gestione della visibilità
 const hide = (elements) => {
     elements.forEach((element) => {
         element.classList.add("hidden");
@@ -11,93 +11,84 @@ const show = (element) => {
     element.classList.remove("hidden");
 };
 
+// Componente Navigatore
 export const createNavigator = (parentElement) => {
     const pages = Array.from(parentElement.querySelectorAll(".page"));
 
     const render = () => {
-        const url = new URL(document.location.href);
-        const pageName = url.hash.replace("#", "") || "pagina1"; // Se non c'è hash, vai su pagina1
+        const pageName = location.hash.replace("#", "") || "pagina1"; 
         const selected = pages.find((page) => page.id === pageName) || pages[0];
 
-        hide(pages); // Nascondi tutte le pagine
-        show(selected); // Mostra la pagina selezionata
+        hide(pages); 
+        show(selected); 
     };
 
-    // Gestisce il caricamento iniziale e il cambio di hash
-    window.addEventListener("hashchange", render); // Reagisci al cambio di hash
-    render(); // Esegui il rendering iniziale
+    window.addEventListener("hashchange", render); 
+    render();
 };
 
-
-//componente mappa
-//creazione iniziale
-let zoom = 12;
-let maxZoom = 19;
-let map = L.map('map').setView([45.4654219,9.1859243], zoom);
+// Componente Mappa
+let map = L.map('map').setView([45.4654219, 9.1859243], 12);
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-maxZoom: maxZoom,
-attribution: '© <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    maxZoom: 19,
+    attribution: '© <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
 
 const renderMap = () => {
     download().then((places) => {
-      places.forEach((place) => {
-        const marker = L.marker(place.coords).addTo(map);
-        marker.bindPopup(`
-            <b>${place.name}</b><br>
-            <b>Data:</b> ${place.date}<br>
-            <b>Ora:</b> ${place.time}<br>
-            <b>Feriti:</b> ${place.injured}<br>
-            <b>Morti:</b> ${place.dead}`);
-      });
-    })
-}
+        places.forEach((place) => {
+            const marker = L.marker(place.coords).addTo(map);
+            marker.bindPopup(`
+                <b>${place.name}</b><br>
+                <b>Data:</b> ${place.date}<br>
+                <b>Ora:</b> ${place.time}<br>
+                <b>Feriti:</b> ${place.injured}<br>
+                <b>Morti:</b> ${place.dead}`);
+        });
+    }).catch(console.error);
+};
+
 // Componente Login
-const createLogin = () => {
+/*const createLogin = () => {
     const inputName = document.querySelector("#name");
     const inputPassword = document.querySelector("#password");
     const loginButton = document.querySelector("#login");
     const divPrivate = document.querySelector("#private");
-    const divLogin = document.querySelector("#login-container"); // Aggiunto un id corretto
+    const divLogin = document.querySelector("#login-container");
 
     let isLogged = sessionStorage.getItem("Logged") === "true";
 
-    // Imposta la visibilità iniziale
     const updateUI = () => {
         if (isLogged) {
             divPrivate.classList.remove("hidden");
-            divPrivate.classList.add("visible");
             divLogin.classList.add("hidden");
         } else {
             divPrivate.classList.add("hidden");
-            divPrivate.classList.remove("visible");
             divLogin.classList.remove("hidden");
         }
     };
 
     updateUI();
 
-    const login = (name, password) => {
-        return fetch("http://ws.cipiaceinfo.it/credential/login", { 
-            method: "POST",
-            headers: {
-                "content-type": "application/json",
-                "key": token // Assumendo che `token` sia definito altrove
-            },
-            body: JSON.stringify({
-                username: name,
-                password: password
-            })
-        })
-        .then(response => response.json())
-        .then(data => data.result)
-        .catch(error => {
+    const login = async (username, password) => {
+        try {
+            const response = await fetch("http://ws.cipiaceinfo.it/credential/login", { 
+                method: "POST",
+                headers: {
+                    "content-type": "application/json",
+                    "key": token
+                },
+                body: JSON.stringify({ username, password })
+            });
+            const data = await response.json();
+            return data.result;
+        } catch (error) {
             console.error("Errore nel login:", error);
             return false;
-        });
+        }
     };
 
-    loginButton.onclick = () => {
+    loginButton.onclick = async () => {
         const username = inputName.value.trim();
         const password = inputPassword.value.trim();
 
@@ -106,19 +97,19 @@ const createLogin = () => {
             return;
         }
 
-        login(username, password).then((result) => {
-            if (result) {
-                isLogged = true;
-                sessionStorage.setItem("Logged", "true");
-                updateUI();
-            } else {
-                alert("Credenziali errate!");
-            }
-        });
+        const result = await login(username, password);
+        if (result) {
+            isLogged = true;
+            sessionStorage.setItem("Logged", "true");
+            updateUI();
+        } else {
+            alert("Credenziali errate!");
+        }
     };
 
     return {
         isLogged: () => isLogged
     };
-};
+};*/
 
+export { renderMap, createLogin, createNavigator };
--- iDocs-master/progetto_finale/main.js
+++ iDocs-master/progetto_finale/main.js
@@ -1,12 +1,7 @@
 
-import { createNavigator } from "./componenti";
+import { createNavigator,renderMap,createLogin } from "./componenti";
 
 
-// Collegamento del navigatore
-document.addEventListener("DOMContentLoaded", () => {
-    const parentElement = document.body;
-    createNavigator(parentElement);
-});
 
 const log = createLogin (document.getElementById("loginContainer"))
 log.setLabels([["Username","text"],["Password","password"]]);
